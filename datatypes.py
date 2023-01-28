f4695adeec0de3305b2ad3ebba8dbc4d93f2d00d
Blackie <109954189+michaelwass@users.noreply.github.com>
2023-01-28 12:11:16 +0200

    Update datatypes.py

--- datatypes.py
+++ datatypes.py
@@ -1,3 +1,9 @@
+#The following program emphasizes the dynamic type system of Python.
+
+#In a dynamically typed language, a variable is simply a value bound to a name.
+
+#The value of what is being referenced by the variable has a type like int or float or str, but the variable itself has no type.
+
 #!/usr/bin/env python3
 x = 10
 tab = " \t"
